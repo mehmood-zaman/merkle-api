2afba63f2a58528dd724d2525651eab915228cd3
mehmood-v <mehmood.zaman@venrup.com>
2022-11-19 16:12:48 +0100

    added comments

--- src/services/ItemssService.ts
+++ src/services/ItemssService.ts
@@ -8,42 +8,41 @@ import config from '../config/Config';
 export default class UserService {
   constructor(public logger: LoggerClient) {}
 
+  //Add new Item
   addNewItem = async (name: string, price: number) => {
     try {
       const newItem = new Item({ name, price });
-
       const savedItem = await newItem.save();
-
       return savedItem;
     } catch (error) {
       throw error;
     }
   };
 
+  //Get an Item
   getAnItem = async (id: string) => {
     try {
       const ItemDetails = await Item.findById({ _id: id });
-
       return ItemDetails;
     } catch (error) {
       throw error;
     }
   };
 
+  //Update an Item
   updateAnItem = async (id: string, name: string, price: number) => {
     try {
       const ItemDetails = await Item.findByIdAndUpdate({ _id: id }, { name, price });
-
       return ItemDetails;
     } catch (error) {
       throw error;
     }
   };
 
+  //Delete an Item
   deleteAnItem = async (id: string) => {
     try {
       const ItemDetails = await Item.findOneAndDelete({ _id: id });
-
       return ItemDetails;
     } catch (error) {
       throw error;
